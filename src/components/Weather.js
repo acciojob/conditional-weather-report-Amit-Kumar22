330cff9f4926bd09304d67e011c5b954ebc09852
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-12-08 11:33:15 +0530

    complted

---
+++ src/components/Weather.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { darken, lighten } from 'polished';
+
+const Weather = ({ temperature, conditions }) => {
+  // Define the temperature threshold
+  const threshold = 20;
+
+  // Determine the color based on the temperature
+  const textColor = temperature > threshold ? 'red' : 'blue';
+
+  // Style object for the text color
+  const textStyle = {
+    color: textColor,
+  };
+
+  // Style object for the background color
+  const containerStyle = {
+    backgroundColor: temperature > threshold ? lighten(0.2, 'red') : lighten(0.2, 'blue'),
+    padding: '20px',
+    borderRadius: '8px',
+  };
+
+  return (
+    <div style={containerStyle}>
+      <h2 style={textStyle}>Current Temperature: {temperature}°C</h2>
+      <p>Conditions: {conditions}</p>
+    </div>
+  );
+};
+
+export default Weather;
